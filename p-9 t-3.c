0d717c06a98972ec4f5d71d3be28f7696c785a34
Dixitkumbhani31 <130464783+Dixitkumbhani31@users.noreply.github.com>
2023-05-25 17:37:04 +0530

    Add files via upload

---
+++ p-9 t-3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+enum months
+
+{
+	Sunday,Monday,Tuseday,Wednesday,Thursday,Friday,Saturday
+};
+
+int main()
+{
+	int a,n,i;
+	for(i=Sunday;i<=Saturday;i++)
+	{
+		printf("%d  ",i);
+	}
+}
